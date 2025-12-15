0f3ed25b294fcff76be379df4a013268516fbb0f
SheltonMark <954174686@qq.com>
2025-12-15 14:48:32 +0800

    feat(ui): add custom-picker and privacy-modal components

--- components/custom-picker/custom-picker.js
+++ components/custom-picker/custom-picker.js
@@ -1,69 +1,31 @@
 Component({
   properties: {
-    // 标签文字
-    label: {
-      type: String,
-      value: ''
-    },
-    // 占位文字
-    placeholder: {
-      type: String,
-      value: '请选择'
-    },
-    // 选项列表 [{id, name}]
-    options: {
-      type: Array,
-      value: []
-    },
-    // 当前选中值
-    value: {
-      type: Number,
-      value: null
-    },
-    // 是否必填
-    required: {
-      type: Boolean,
-      value: false
-    },
-    // 是否禁用
-    disabled: {
-      type: Boolean,
-      value: false
-    }
+    options: { type: Array, value: [] },
+    value: { type: String, value: '' },
+    placeholder: { type: String, value: '请选择' },
+    disabled: { type: Boolean, value: false }
   },
-
   data: {
     isOpen: false,
-    selectedItem: null
+    selectedLabel: ''
   },
-
   observers: {
     'value, options': function(value, options) {
-      if (value && options && options.length > 0) {
-        const item = options.find(opt => opt.id === value);
-        this.setData({ selectedItem: item || null });
-      } else {
-        this.setData({ selectedItem: null });
-      }
+      const selected = options.find(opt => opt.value === value);
+      this.setData({ selectedLabel: selected ? selected.label : '' });
     }
   },
-
   methods: {
-    toggleDropdown() {
+    onTapTrigger() {
       if (this.data.disabled) return;
       this.setData({ isOpen: !this.data.isOpen });
     },
-
-    selectOption(e) {
-      const { id, name } = e.currentTarget.dataset;
-      this.setData({
-        selectedItem: { id, name },
-        isOpen: false
-      });
-      this.triggerEvent('change', { value: id, item: { id, name } });
+    onSelectOption(e) {
+      const { value } = e.currentTarget.dataset;
+      this.setData({ isOpen: false });
+      this.triggerEvent('change', { value });
     },
-
-    closeDropdown() {
+    onClose() {
       this.setData({ isOpen: false });
     }
   }
--- components/privacy-modal/privacy-modal.js
+++ components/privacy-modal/privacy-modal.js
@@ -1,40 +1,16 @@
 Component({
   properties: {
-    // 是否显示
-    show: {
-      type: Boolean,
-      value: false
-    }
+    show: { type: Boolean, value: false }
   },
-
-  data: {},
-
   methods: {
-    // 同意
     onAgree() {
       this.triggerEvent('agree');
     },
-
-    // 拒绝
     onDisagree() {
       this.triggerEvent('disagree');
     },
-
-    // 查看用户协议
-    viewUserAgreement() {
-      wx.navigateTo({
-        url: '/pages/privacy/privacy?type=user-agreement'
-      });
-    },
-
-    // 查看隐私政策
-    viewPrivacyPolicy() {
-      wx.navigateTo({
-        url: '/pages/privacy/privacy?type=privacy-policy'
-      });
-    },
-
-    // 阻止冒泡
-    preventBubble() {}
+    onTapPrivacy() {
+      wx.navigateTo({ url: '/pages/privacy/privacy' });
+    }
   }
 });
